ec3f5defbabb121368a874a963122983f860d492
ahmetyanik <ahmetyank4242@gmail.com>
2022-02-02 13:03:12 +0100

    cors

--- Mockdatabase/src/index.js
+++ Mockdatabase/src/index.js
@@ -3,7 +3,7 @@ import { addRecord, getRecords, showFour } from "./controllers/records.controlle
 import cors from "cors";
 
 const corsOptions ={
-  origin:'http://localhost:3000', 
+  origin:'*', 
   credentials:true,            //access-control-allow-credentials:true
   optionSuccessStatus:200,
 }
